e1e929203dd9210f6e4d0757b1795756f26d4844
runiona <runiona5052@student.faytechcc.edu>
2026-09-03 18:05:51 +0000

    p1lab2_start

---
+++ p1lab1_runion.py
@@ -0,0 +1,11 @@
+# Aiden Runion
+# 9/3/26
+# First Python program
+
+print("Hello!")
+
+first_name = input("What's your first name? ")
+
+last_name = input("What's your last name? ")
+
+print("Nice to meet you", first_name, last_name)
---
+++ p1lab2_runion.py
@@ -0,0 +1,19 @@
+# CTI 110
+# P1LAB2 - Selling Things
+# Runiona
+# 9/3/26
+
+# Fictional Store -- pick three things 
+# product_name, product_count, product_price
+
+#change these to own values
+product_name = "CPU"
+product_count = 400
+product_price = 239.99
+
+print("Welcome to the", product_name, "store")
+print("we have", product_count, product_name, "(s) at $", product_price, "each.")
+
+total = product_count * product_price
+
+print("Total is: $",total)
