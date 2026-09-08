41e15a50c44633db5c4f408a20e2c30261da2b3e
runiona <runiona5052@student.faytechcc.edu>
2026-09-08 18:01:24 +0000

    M1HW1 part 2

---
+++ p1hw1_runion.py
@@ -0,0 +1,19 @@
+# CTI 110
+# P1HW1 - Math
+# Runion, A
+# 9/8/26
+# Do some math processing
+
+# PART 1 - EXPONENTS
+
+# PART 2 - ADDITION SUBTRACTION
+# 3 numbers, start, add_this, sub_this
+start = int(input("Enter the starting integer: "))
+#print("you typed", start)
+add_this = int(input("Enter integer to add: "))
+sub_this = int(input("Enter integer to subtract: "))
+answer = start + add_this - sub_this
+# print the answer
+print("\n\n"); 
+print(start, "+", add_this, "-", sub_this, "is equal to", answer)
+
