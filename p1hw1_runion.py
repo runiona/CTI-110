57c35c512a19ef8d5060b8ac3ab12fb21071b81b
runiona <runiona5052@student.faytechcc.edu>
2026-09-08 18:10:41 +0000

    p1hw1 done

--- p1hw1_runion.py
+++ p1hw1_runion.py
@@ -5,8 +5,17 @@
 # Do some math processing
 
 # PART 1 - EXPONENTS
+print("-----Calculating Exponents-----")
+print("\n") # 2 newlines
+base = int(input("Enter integer as base value: "))
+exponent = int(input("Enter integer as exponent: "))
+result = base ** exponent
+print(f"{base} to the {exponent} power is {result} !!")
+
 
 # PART 2 - ADDITION SUBTRACTION
+print("-----Addition and Subtraction-----")
+print("\n") # 2 newlines
 # 3 numbers, start, add_this, sub_this
 start = int(input("Enter the starting integer: "))
 #print("you typed", start)
